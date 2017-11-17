c29f65b5fe1c50e738f9fc4c0c0465681497bc24
Rauch Cornelia <rauchcor13@r00307.fh-joanneum.at>
2017-11-17 20:17:07 +0100

    LocationCalculator

---
+++ Compass.me/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Compass.me
+//
+//  Created by Rauch Cornelia on 17/11/2017.
+//  Copyright © 2017 Cemi Rrahel. All rights reserved.
+//
+
+import Foundation
---
+++ Compass.me/LocationAdjuster.swift
@@ -0,0 +1,9 @@
+//
+//  LocationAdjuster.swift
+//  Compass.me
+//
+//  Created by Rauch Cornelia on 17/11/2017.
+//  Copyright © 2017 Cemi Rrahel. All rights reserved.
+//
+
+import Foundation
