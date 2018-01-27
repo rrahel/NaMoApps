89bbf4ff42619a85d0bf4ff6ee9a135bb24d7d6d
Rauch Cornelia <rauchcor13@r00308.fh-joanneum.at>
2018-01-27 09:11:37 +0100

    refactor: moved display spinner to helper class

--- Compass.me/ViewController.swift
+++ Compass.me/ViewController.swift
@@ -88,25 +88,4 @@ class ViewController: UIViewController {
         print("checking name")
         sendLogin(username: username_field.text!)
     }
-    
-    private func displaySpinner(onView : UIView) -> UIView {
-        let spinnerView = UIView.init(frame: onView.bounds)
-        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
-        let ai = UIActivityIndicatorView.init(activityIndicatorStyle: .whiteLarge)
-        ai.startAnimating()
-        ai.center = spinnerView.center
-        
-        DispatchQueue.main.async {
-            spinnerView.addSubview(ai)
-            onView.addSubview(spinnerView)
-        }
-        
-        return spinnerView
-    }
-    
-    private func removeSpinner(spinner :UIView) {
-        DispatchQueue.main.async {
-            spinner.removeFromSuperview()
-        }
-    }
 }
